7ba01b6b33bd2e29e87e6fa6dd29dd3f660b434b
MGSHLAPMAC <sharly2012@sina.com>
2018-03-12 18:41:43 +0800

    Change

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,12 @@
 public class Test {
 
 	public static void main(String[] args) throws Exception {
-		String text = "http://www.dans88.com.cn";
+		String text = "http://www.baidu.com";
 		QRCodeUtil.encode(text, "", "/Users/admin/test", true);
 	}
 	
 	// public static void main(String[] args) throws Exception {
-	// String text = "http://www.dans88.com.cn";
+	// String text = "http://www.baidu.com";
 	// QRCodeUtil.encode(text, "/Users/admin/test/my180.jpg", "/Users/admin/test",
 	// true);
 	// }
