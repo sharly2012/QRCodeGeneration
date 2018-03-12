39aa2adccc3b771a849427ac99ec185ec39b8b6c
sharly2012 <sharly2012@sina.com>
2018-03-12 18:30:42 +0800

    test

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 		String text = "http://www.dans88.com.cn";
 		QRCodeUtil.encode(text, "", "/Users/admin/test", true);
 	}
+	
 	// public static void main(String[] args) throws Exception {
 	// String text = "http://www.dans88.com.cn";
 	// QRCodeUtil.encode(text, "/Users/admin/test/my180.jpg", "/Users/admin/test",
